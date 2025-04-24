cf36fbc6c7d225e24a7560caef430e9a31f55efd
cy23259@shibaura-it.ac.jp <cy23259@shibaura-it.ac.jp>
2025-04-24 14:03:41 +0900

    altered

--- file.c
+++ file.c
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 
 int main(){
-  char buff[12];
+  char buff[24];
   int fd;
   int val1,val2;
-  fd = open("test.txt",0,O_RDONLY);
-  val1 = read(fd,buff,12);
-  val2 = write(1,buff,12);
+  fd = open("master.txt",0,O_RDONLY);
+  val1 = read(fd,buff,24);
+  val2 = write(1,buff,24);
   if(val1 < 0 || val2 < 0){
     perror("");
     exit(1);
